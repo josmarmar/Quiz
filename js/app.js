29f4a6850bf3a6b8da23eff0820491f7c51345a7
JoseMarin <josem_marin@hotmail.com>
2021-02-23 12:58:02 +0100

    Ich habe geschafft und es hat geklappt! JS!

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-// -----------------vars&const---------------------------
-
-// El formulario
-// let form = document.getElementById(formulario);
-
-// Las respuestas correctas son:
-// const rightAnsw = ["a14", "a23", "a33", "a44", "a52"];
-
-// Puntos
-
-// let score = 0;
-
-// Validador de respuestas correctas y completas
 
 function validator(){
 let form = document.forms["formulario"]; 
@@ -21,7 +8,7 @@ let form = document.forms["formulario"];
 for(var i = 1; i <= question; i++){
     if(form["question" + i].value === "" || form["question" + i].value === null){
         window.alert("Debe responder todas las preguntas");
-        console.log("Has fallado la pregunta " + i)
+        
         return false;
     }else{
         if(form["question" + i].value === rightAnsw[i-1]){
@@ -36,23 +23,15 @@ window.alert("Has conseguido " + score +"/5");
 return false;
 }
 
-// El submit
+// Empecé el proyecto con <div>s, pero el data-value se me hizo un mundo para hacer solo 20 preguntas, así que pasé al formulario. Lo que supuso tener que cambiar el CSS de la pág index.html.
 
-// function formSubmit()
-// {
-//     form.submit();
-// }
+// form.getElementById no funcionó, me hizo cambiar el código de toda la función, cambiar los valores del array rightAnsw porque pensaba que los value no coincidían con el form. La solución fue document.forms[], aunque también me dio dolores de cabeza porque escribí form, sin "s" y estaba haciendo referencia a la variable form.
 
-
-// NonAutoRefresh
-
-// let btn = document.getElementById("submitbtn");
-
-// btn.addEventListener("click", function(removEvent) {removEvent.preventDefault();});
-
-// y no funciona, y sigue sin funcionar
+// Dios bendiga internet por enseñarme el atributo "onsubmit", aunque más tarde de poco me sirvió para evitar el refresco de la página con preventDefault();. Me planteé hasta usar Ajax (no joke). Al final, al menos así me parece, he podido evitar el autorefresco con el window.alert. 
 
 
+
+ 
 
 
 
