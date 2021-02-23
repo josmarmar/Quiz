bc68084bc3770ab66eca2615d20597790912b364
JoseMarin <josem_marin@hotmail.com>
2021-02-23 09:05:18 +0100

    4 attmpt

--- js/app.js
+++ js/app.js
@@ -12,29 +12,39 @@ let score = 0;
 
 // Validador de respuestas correctas y completas
 
-for(var i = 1; i < rightAnsw.length; i++){
-    if(form["question" + i].value = ""){
+function validator(){
+   
+
+for(var i = 1; i <= rightAnsw.length; i++){
+    if(form["question" + i].value === "" || form["question" + i].value === null){
         window.alert("Debe responder todas las preguntas");
         return false;
     }else{
-        if(form["question" + i].value = rightAnsw[i-1]){
-            return score++;
+        if(form["question" + i].value === rightAnsw[i-1]){
+            score++;
+            console.log("AllRite")
         }
     } 
 
 }
-
-
+return false;
+}
 
 // El submit
 
-function formSubmit()
-{
-    form.submit();
-}
+// function formSubmit()
+// {
+//     form.submit();
+// }
 
 
+// NonAutoRefresh
 
+// let btn = document.getElementById("submitbtn");
+
+// btn.addEventListener("click", function(removEvent) {removEvent.preventDefault();});
+
+// y no funciona, y sigue sin funcionar
 
 
 
@@ -79,8 +89,6 @@ window.alert("Has conseguido " + score +"/5");
 
 
 
-
-// Pero, todas las preguntas han de ser respondidas! (min6:40)
 
 
 
